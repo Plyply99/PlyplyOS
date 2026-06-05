40f6d7933955003282808cbc7fc5bca21995f677
Plyply99 <103620143+Plyply99@users.noreply.github.com>
2026-06-05 07:54:18 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,27 +5,18 @@ set -ouex pipefail
 # Add repos
 dnf5 -y copr enable avengemedia/dms-git 
 dnf5 -y copr enable yalter/niri-git
-dnf5 -y copr enable ublue-os/akmods 
 # Terra repo
 dnf -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-
 
 # Use niri-git instead of fedora repo
 echo "priority=1" | tee -a /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri-git.repo
 
 ### Install packages
-dnf5 -y install bat bat-extras btop cava chafa cliphist dgop dms emacs eza fastfetch ghostty-nightly grim input-remapper kernel-devel mangohud niri rpmdevtools slurp vkBasalt 
+dnf5 -y install bat bat-extras btop cava chafa cliphist dgop dms emacs eza fastfetch ghostty-nightly grim input-remapper mangohud niri slurp vkBasalt 
 # akmods
 
 # Remove niri-git optional dependencies
 dnf5 -y remove alacritty fuzzel mako swaybg swayidle swaylock waybar
-
-
-# Use a COPR Example:
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
 
 #### Example for enabling a System Unit File
 #systemctl enable podman.socket
