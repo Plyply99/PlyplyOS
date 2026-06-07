b5aea0b98b02f7a379236ac1da0a453d7ff93aec
Plyply99 <103620143+Plyply99@users.noreply.github.com>
2026-06-07 08:36:31 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,12 +11,22 @@ dnf -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/ter
 # Use niri-git instead of fedora repo
 echo "priority=1" | tee -a /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri-git.repo
 
+# Install support packages - printing bluetooth etc
+dnf5 -y install cups cups-pk-helper system-config-printer bluez bluez-tools bluetool bluez-cups
+# bluez bluez-tools bluetool bluez-cups
+
 ### Install packages
-dnf5 -y install bat bat-extras btop cava chafa cliphist dgop dms emacs eza fastfetch ghostty-nightly grim input-remapper mangohud niri slurp vkBasalt 
-# akmods
+dnf5 -y install bat bat-extras btop cava chafa cliphist dgop dms dms-greeter dsearch emacs eza fastfetch ghostty-nightly grim htop input-remapper mangohud niri qt6-multimedia slurp vkBasalt wl-clipboard
+dnf5 -y install thunar thunar-archive-plugin thunar-media-tags-plugin thunar-vcs-plugin thunar-volman
 
 # Remove niri-git optional dependencies
 dnf5 -y remove alacritty fuzzel mako swaybg swayidle swaylock waybar
 
 #### Example for enabling a System Unit File
 #systemctl enable podman.socket
+systemctl enable cups.socket
+
+# Setting up DMS Greeter
+dms greeter enable
+dms greeter sync
+
