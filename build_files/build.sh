e3a76f7b38a70925a9fcb6789b8a19a731145fbf
Plyply99 <103620143+Plyply99@users.noreply.github.com>
2026-06-07 13:27:25 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -18,7 +18,7 @@ dnf5 -y install cups-pk-helper system-config-printer
 ### Install packages
 dnf5 -y install adw-gtk3-theme bat bat-extras btop cava chafa cliphist dgop dms dms-greeter dsearch emacs eza fastfetch ghostty-nightly gnome-disk-utility grim input-remapper mangohud niri nwg-look qt6-qtmultimedia slurp vkBasalt
 # File manager
-dnf5 -y install file-roller thunar thunar-archive-plugin thunar-media-tags-plugin thunar-vcs-plugin thunar-volman
+dnf5 -y install file-roller thunar thunar-archive-plugin thunar-volman
 # Gnome software center
 dnf5 -y install gnome-software gnome-software-rpm-ostree
 
