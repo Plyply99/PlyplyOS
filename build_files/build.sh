f8fcf74ae78a329a7617077debd3193216e62975
Plyply99 <103620143+Plyply99@users.noreply.github.com>
2026-06-13 11:03:32 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,9 +11,8 @@ dnf -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/ter
 echo "priority=1" | tee -a /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri-git.repo
 
 ### Install packages
-dnf5 -y install adw-gtk3-theme bat bat-extras btop chafa cliphist dgop dms dms-greeter dsearch emacs eza fastfetch ghostty gnome-disk-utility grim input-remapper mangohud mpv nautilus niri nwg-look qt6-qtmultimedia slurp vkBasalt
-dnf5 -y remove cava
-dnf5 -y  --disablerepo="*" --enablerepo=fedora --enablerepo=updates install cava   
+dnf5 -y install adw-gtk3-theme bat bat-extras btop cava chafa cliphist dgop dms dms-greeter dsearch emacs eza fastfetch ghostty gnome-disk-utility grim input-remapper mangohud mpv nautilus niri nwg-look qt6-qtmultimedia slurp vkBasalt
+   
 # Gnome software center
 dnf5 -y install gnome-software gnome-software-rpm-ostree
 
