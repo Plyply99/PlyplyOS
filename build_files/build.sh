ecec6a50b7957036f357cff21ea7fa082804d59a
Plyply99 <103620143+Plyply99@users.noreply.github.com>
2026-06-13 11:02:07 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,10 +2,8 @@
 
 set -ouex pipefail
 
-# Add copr repos
 dnf5 -y copr enable avengemedia/dms-git 
 dnf5 -y copr enable yalter/niri-git
-dnf5 -y copr enable nett00n/hyprland 
 # Terra repo
 dnf -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 
@@ -13,14 +11,18 @@ dnf -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/ter
 echo "priority=1" | tee -a /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri-git.repo
 
 ### Install packages
-dnf5 -y install hyprland hyprpicker uwsm
 dnf5 -y install adw-gtk3-theme bat bat-extras btop chafa cliphist dgop dms dms-greeter dsearch emacs eza fastfetch ghostty gnome-disk-utility grim input-remapper mangohud mpv nautilus niri nwg-look qt6-qtmultimedia slurp vkBasalt
+dnf5 -y remove cava
 dnf5 -y  --disablerepo="*" --enablerepo=fedora --enablerepo=updates install cava   
 # Gnome software center
 dnf5 -y install gnome-software gnome-software-rpm-ostree
 
-# Remove niri-git optional dependencies
-dnf5 -y remove alacritty fuzzel mako swaybg swayidle swaylock SwayNotificationCenter waybar
+# Hyprland
+dnf5 -y copr enable nett00n/hyprland 
+dnf5 -y install hyprland hyprland-guiutils hyprpicker uwsm
+
+# Remove niri-git and hyprland optional dependencies
+dnf5 -y remove alacritty fuzzel mako nwg-panel swaybg swayidle swaylock SwayNotificationCenter waybar
 
 #### Example for enabling a System Unit File
 #systemctl enable podman.socket
