2655b10a4023c23a60545e5a7657aded578f3de5
Plyply99 <103620143+Plyply99@users.noreply.github.com>
2026-07-10 14:09:48 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -55,9 +55,6 @@ localedef -i en_US -f UTF-8 en_US.UTF-8
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 sh -c 'echo -e "PATH=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin\"\nLANG=\"en_US.UTF-8\"" > /etc/environment'
 
-# rpcbind stuff
-mkdir -p /usr/lib/tmpfiles.d && echo 'd /run/rpcbind 0700 rpc rpc -' > /usr/lib/tmpfiles.d/rpcbind.conf
-
 # Set os-release
 HOME_URL="https://github.com/Plyply99/PlyplyOS"
 sed -i -f - /usr/lib/os-release <<EOF
